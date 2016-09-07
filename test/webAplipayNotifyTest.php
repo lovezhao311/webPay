ad98c102a1320e8be9cd6d60db17ffc6c25ce27c
luffyzhao <luffyzhao@vip.126.com>
2016-09-07 21:25:53 +0800

    测试 notifyView

--- test/webAplipayNotifyTest.php
+++ test/webAplipayNotifyTest.php
@@ -9,12 +9,14 @@ use \LuffyZhao\Exception\PayException;
 $config = include '../src/Config/webAplipay.php';
 
 try {
-    $verify = \LuffyZhao\Pay::instance('WebAlipay', $config)->notifyVerify();
+    $pay = \LuffyZhao\Pay::instance('WebAlipay', $config);
+
+    $verify = $pay->notifyVerify();
 
     if ($verify['status'] == 0) {
         throw new PayException("支付失败！");
     } elseif ($verify['status'] == 1) {
-        // 支付成功
+        echo $pay->notifyView();
     }
 
 } catch (PayException $e) {
